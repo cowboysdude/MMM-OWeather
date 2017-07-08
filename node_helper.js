90fbe20b8ed6027d486007e4452e27747b904007
John Wade <cowboysdude@stny.rr.com>
2017-07-07 21:52:06 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,4 @@
 
-  
   /* Magic Mirror
    * Module: MMM-OWeather
    *
@@ -8,6 +7,7 @@
    */
 var NodeHelper = require('node_helper');
 const request = require('request');
+var moment = require('moment-timezone');
 
 module.exports = NodeHelper.create({
 	  
@@ -23,16 +23,30 @@ module.exports = NodeHelper.create({
             if (!error && response.statusCode === 200) {
                         var result = JSON.parse(body);
                         this.sendSocketNotification("WEATHER_RESULT", result);
+                        this.getSRSS();
+            }
+       });
+    },
+     getSRSS: function(){
+     	var self = this;
+	 	request({ 
+    	    url: "https://api.sunrise-sunset.org/json?lat=42.089796&lng=-76.807734&formatted=0",
+    	          method: 'GET' 
+    	        }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                        var srssresult = JSON.parse(body);
+                        this.sendSocketNotification("SRSS_RESULTS", srssresult);
             }
        });
     },
     
-    
-
     //Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_WEATHER') {
                 this.getWeather(payload);
             }
+         if (notification === 'GET_SRSS') {
+                this.getSRSS(payload);
+			}    
          }  
     });
