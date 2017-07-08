fa6461bca8e88aae6f85fef3eff6edcf4003fca7
John Wade <cowboysdude@stny.rr.com>
2017-07-08 07:31:46 -0400

    Update MMM-OWeather.js

--- MMM-OWeather.js
+++ MMM-OWeather.js
@@ -22,105 +22,105 @@
           uac: "PLEASE GET ENTER OR GET A UAC KEY AT http://www.myweather2.com/developer/",
           
           iconTableDay: {
-            "0": "modules/MMM-Oweather/icons/sunny.png",
-            "1": "modules/MMM-Oweather/icons/PartlyCloudyDay.png",
-            "2": "modules/MMM-Oweather/icons/cloudy.png",
-            "3": "modules/MMM-Oweather/icons/Overcast.png",
-            "10": "modules/MMM-Oweather/icons/Mist.png",
-            "21": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "22": "modules/MMM-Oweather/icons/IsoSleetSwrsDay.png",
-            "23": "modules/MMM-Oweather/icons/OccLightSleet.png",
-            "24": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "29": "modules/MMM-Oweather/icons/PartCloudRainThunderDay.png",
-            "38": "modules/MMM-Oweather/icons/ModSnow.png",
-            "39": "modules/MMM-Oweather/icons/Blizzard.png",
-            "45": "modules/MMM-Oweather/icons/Fog.png",
-            "49": "modules/MMM-Oweather/icons/FreezingFog.png",
-            "50": "modules/MMM-Oweather/icons/IsoRainSwrsDay.png",
-            "51": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "56": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "57": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "60": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "61": "modules/MMM-Oweather/icons/ModRain.png",
-            "62": "modules/MMM-Oweather/icons/ModRainSwrsDay.png",
-            "63": "modules/MMM-Oweather/icons/ModRain.png",
-            "64": "modules/MMM-Oweather/icons/HeavyRainSwrsDay.png",
-            "65": "modules/MMM-Oweather/icons/HeavyRain.png",
-            "66": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "67": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "68": "modules/MMM-Oweather/icons/ModSleet.png",
-            "69": "modules/MMM-Oweather/icons/HeavySleet.png",
-            "70": "modules/MMM-Oweather/icons/OccLightSnow.png",
-            "71": "modules/MMM-Oweather/icons/OccLightSnow.png",
-            "72": "modules/MMM-Oweather/icons/ModSnow.png",
-            "73": "modules/MMM-Oweather/icons/ModSnow.png",
-            "74": "modules/MMM-Oweather/icons/HeavySnowSwrsDay.png",
-            "75": "modules/MMM-Oweather/icons/HeavySnow.png",
-            "79": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "80": "modules/MMM-Oweather/icons/IsoRainSwrsDay.png",
-            "81": "modules/MMM-Oweather/icons/ModRainSwrsDay.png",
-            "82": "modules/MMM-Oweather/icons/HeavyRain.png",
-            "83": "modules/MMM-Oweather/icons/ModSleetSwrsDay.png",
-            "84": "modules/MMM-Oweather/icons/ModSleetSwrsDay.png",
-            "85": "modules/MMM-Oweather/icons/IsoSnowSwrsDay.png",
-            "86": "modules/MMM-Oweather/icons/ModSnowSwrsDay.png",
-            "87": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "88": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "91": "modules/MMM-Oweather/icons/PartCloudRainThunderDay.png",
-            "92": "modules/MMM-Oweather/icons/CloudRainThunder.png",
-            "93": "modules/MMM-Oweather/icons/PartCloudSleetSnowThunderDay.png",
-            "94": "modules/MMM-Oweather/icons/CloudSleetSnowThunder.png"
+            "0": "modules/MMM-OWeather/icons/sunny.png",
+            "1": "modules/MMM-OWeather/icons/PartlyCloudyDay.png",
+            "2": "modules/MMM-OWeather/icons/cloudy.png",
+            "3": "modules/MMM-OWeather/icons/Overcast.png",
+            "10": "modules/MMM-OWeather/icons/Mist.png",
+            "21": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "22": "modules/MMM-OWeather/icons/IsoSleetSwrsDay.png",
+            "23": "modules/MMM-OWeather/icons/OccLightSleet.png",
+            "24": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "29": "modules/MMM-OWeather/icons/PartCloudRainThunderDay.png",
+            "38": "modules/MMM-OWeather/icons/ModSnow.png",
+            "39": "modules/MMM-OWeather/icons/Blizzard.png",
+            "45": "modules/MMM-OWeather/icons/Fog.png",
+            "49": "modules/MMM-OWeather/icons/FreezingFog.png",
+            "50": "modules/MMM-OWeather/icons/IsoRainSwrsDay.png",
+            "51": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "56": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "57": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "60": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "61": "modules/MMM-OWeather/icons/ModRain.png",
+            "62": "modules/MMM-OWeather/icons/ModRainSwrsDay.png",
+            "63": "modules/MMM-OWeather/icons/ModRain.png",
+            "64": "modules/MMM-OWeather/icons/HeavyRainSwrsDay.png",
+            "65": "modules/MMM-OWeather/icons/HeavyRain.png",
+            "66": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "67": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "68": "modules/MMM-OWeather/icons/ModSleet.png",
+            "69": "modules/MMM-OWeather/icons/HeavySleet.png",
+            "70": "modules/MMM-OWeather/icons/OccLightSnow.png",
+            "71": "modules/MMM-OWeather/icons/OccLightSnow.png",
+            "72": "modules/MMM-OWeather/icons/ModSnow.png",
+            "73": "modules/MMM-OWeather/icons/ModSnow.png",
+            "74": "modules/MMM-OWeather/icons/HeavySnowSwrsDay.png",
+            "75": "modules/MMM-OWeather/icons/HeavySnow.png",
+            "79": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "80": "modules/MMM-OWeather/icons/IsoRainSwrsDay.png",
+            "81": "modules/MMM-OWeather/icons/ModRainSwrsDay.png",
+            "82": "modules/MMM-OWeather/icons/HeavyRain.png",
+            "83": "modules/MMM-OWeather/icons/ModSleetSwrsDay.png",
+            "84": "modules/MMM-OWeather/icons/ModSleetSwrsDay.png",
+            "85": "modules/MMM-OWeather/icons/IsoSnowSwrsDay.png",
+            "86": "modules/MMM-OWeather/icons/ModSnowSwrsDay.png",
+            "87": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "88": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "91": "modules/MMM-OWeather/icons/PartCloudRainThunderDay.png",
+            "92": "modules/MMM-OWeather/icons/CloudRainThunder.png",
+            "93": "modules/MMM-OWeather/icons/PartCloudSleetSnowThunderDay.png",
+            "94": "modules/MMM-OWeather/icons/CloudSleetSnowThunder.png"
         },
 
         iconTableNight: {
-            "0": "modules/MMM-Oweather/icons/clear.png",
-            "1": "modules/MMM-Oweather/icons/PartyCloudyNight.png",
-            "2": "modules/MMM-Oweather/icons/cloudy.png",
-            "3": "modules/MMM-Oweather/icons/Overcast.png",
-            "10": "modules/MMM-Oweather/icons/Mist.png",
-            "21": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "22": "modules/MMM-Oweather/icons/IsoSleetSwrsNight.png",
-            "23": "modules/MMM-Oweather/icons/OccLightSleet.png",
-            "24": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "29": "modules/MMM-Oweather/icons/PartCloudRainThunderNight.png",
-            "38": "modules/MMM-Oweather/icons/ModSnow.png",
-            "39": "modules/MMM-Oweather/icons/Blizzard.png",
-            "45": "modules/MMM-Oweather/icons/Fog.png",
-            "49": "modules/MMM-Oweather/icons/FreezingFog.png",
-            "50": "modules/MMM-Oweather/icons/IsoRainSwrsNight.png",
-            "51": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "56": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "57": "modules/MMM-Oweather/icons/FreezingDrizzle.png",
-            "60": "modules/MMM-Oweather/icons/OccLightRain.png",
-            "61": "modules/MMM-Oweather/icons/ModRain.png",
-            "62": "modules/MMM-Oweather/icons/ModRainSwrsNight.png",
-            "63": "modules/MMM-Oweather/icons/ModRain.png",
-            "64": "modules/MMM-Oweather/icons/HeavyRainSwrsNight.png",
-            "65": "modules/MMM-Oweather/icons/HeavyRain.png",
-            "66": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "67": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "68": "modules/MMM-Oweather/icons/ModSleet.png",
-            "69": "modules/MMM-Oweather/icons/HeavySleet.png",
-            "70": "modules/MMM-Oweather/icons/OccLightSnow.png",
-            "71": "modules/MMM-Oweather/icons/OccLightSnow.png",
-            "72": "modules/MMM-Oweather/icons/ModSnow.png",
-            "73": "modules/MMM-Oweather/icons/ModSnow.png",
-            "74": "modules/MMM-Oweather/icons/HeavySnowSwrsNight.png",
-            "75": "modules/MMM-Oweather/icons/HeavySnow.png",
-            "79": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "80": "modules/MMM-Oweather/icons/IsoRainSwrsNight.png",
-            "81": "modules/MMM-Oweather/icons/ModRainSwrsNight.png",
-            "82": "modules/MMM-Oweather/icons/HeavyRain.png",
-            "83": "modules/MMM-Oweather/icons/ModSleetSwrsNight.png",
-            "84": "modules/MMM-Oweather/icons/ModSleetSwrsNight.png",
-            "85": "modules/MMM-Oweather/icons/IsoSnowSwrsNight.png",
-            "86": "modules/MMM-Oweather/icons/ModSnowSwrsNight.png",
-            "87": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "88": "modules/MMM-Oweather/icons/FreezingRain.png",
-            "91": "modules/MMM-Oweather/icons/PartCloudRainThunderNight.png",
-            "92": "modules/MMM-Oweather/icons/CloudRainThunder.png",
-            "93": "modules/MMM-Oweather/icons/PartCloudSleetSnowThunderNight.png",
-            "94": "modules/MMM-Oweather/icons/CloudSleetSnowThunder.png"
+            "0": "modules/MMM-OWeather/icons/clear.png",
+            "1": "modules/MMM-OWeather/icons/PartyCloudyNight.png",
+            "2": "modules/MMM-OWeather/icons/cloudy.png",
+            "3": "modules/MMM-OWeather/icons/Overcast.png",
+            "10": "modules/MMM-OWeather/icons/Mist.png",
+            "21": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "22": "modules/MMM-OWeather/icons/IsoSleetSwrsNight.png",
+            "23": "modules/MMM-OWeather/icons/OccLightSleet.png",
+            "24": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "29": "modules/MMM-OWeather/icons/PartCloudRainThunderNight.png",
+            "38": "modules/MMM-OWeather/icons/ModSnow.png",
+            "39": "modules/MMM-OWeather/icons/Blizzard.png",
+            "45": "modules/MMM-OWeather/icons/Fog.png",
+            "49": "modules/MMM-OWeather/icons/FreezingFog.png",
+            "50": "modules/MMM-OWeather/icons/IsoRainSwrsNight.png",
+            "51": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "56": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "57": "modules/MMM-OWeather/icons/FreezingDrizzle.png",
+            "60": "modules/MMM-OWeather/icons/OccLightRain.png",
+            "61": "modules/MMM-OWeather/icons/ModRain.png",
+            "62": "modules/MMM-OWeather/icons/ModRainSwrsNight.png",
+            "63": "modules/MMM-OWeather/icons/ModRain.png",
+            "64": "modules/MMM-OWeather/icons/HeavyRainSwrsNight.png",
+            "65": "modules/MMM-OWeather/icons/HeavyRain.png",
+            "66": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "67": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "68": "modules/MMM-OWeather/icons/ModSleet.png",
+            "69": "modules/MMM-OWeather/icons/HeavySleet.png",
+            "70": "modules/MMM-OWeather/icons/OccLightSnow.png",
+            "71": "modules/MMM-OWeather/icons/OccLightSnow.png",
+            "72": "modules/MMM-OWeather/icons/ModSnow.png",
+            "73": "modules/MMM-OWeather/icons/ModSnow.png",
+            "74": "modules/MMM-OWeather/icons/HeavySnowSwrsNight.png",
+            "75": "modules/MMM-OWeather/icons/HeavySnow.png",
+            "79": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "80": "modules/MMM-OWeather/icons/IsoRainSwrsNight.png",
+            "81": "modules/MMM-OWeather/icons/ModRainSwrsNight.png",
+            "82": "modules/MMM-OWeather/icons/HeavyRain.png",
+            "83": "modules/MMM-OWeather/icons/ModSleetSwrsNight.png",
+            "84": "modules/MMM-OWeather/icons/ModSleetSwrsNight.png",
+            "85": "modules/MMM-OWeather/icons/IsoSnowSwrsNight.png",
+            "86": "modules/MMM-OWeather/icons/ModSnowSwrsNight.png",
+            "87": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "88": "modules/MMM-OWeather/icons/FreezingRain.png",
+            "91": "modules/MMM-OWeather/icons/PartCloudRainThunderNight.png",
+            "92": "modules/MMM-OWeather/icons/CloudRainThunder.png",
+            "93": "modules/MMM-OWeather/icons/PartCloudSleetSnowThunderNight.png",
+            "94": "modules/MMM-OWeather/icons/CloudSleetSnowThunder.png"
         },
           
       },
@@ -164,7 +164,7 @@
 		  
           if (!this.loaded) {
              wrapper.classList.add("wrapper", "open");
-             wrapper.innerHTML = "Forecasting ...";
+             wrapper.innerHTML = "Forecasting Weather...";
              wrapper.className = "bright light small";
              return wrapper;
 			}  
@@ -177,6 +177,8 @@
 			} else {
           
           var weather = this.weather;
+          
+          
           
           var forecast = this.weather.forecast;
           var keys = Object.keys(this.weather.forecast);
@@ -196,7 +198,7 @@
             
             var locationImg = document.createElement("th");
 			locationImg.setAttribute("colspan", 1);
-			locationImg.classList.add("xsmall","bright");
+			locationImg.classList.add("small","bright");
 			locationImg.innerHTML = "Currently";
 			locationRow.appendChild(locationImg);
 			weatherTable.appendChild(locationRow);
@@ -224,7 +226,7 @@
 			
 			var nextFive = document.createElement("th");
 			nextFive.setAttribute("colspan", 1);
-			nextFive.classList.add("xsmall","bright");
+			nextFive.classList.add("small","bright");
 			nextFive.innerHTML = "Extended Forecast";
 			locationRow.appendChild(nextFive);
 			weatherTable.appendChild(locationRow);
@@ -278,27 +280,27 @@
             weatherTable.appendChild(row);
 			
 			var dateColumn = document.createElement("td");
-			dateColumn.setAttribute('style', 'font-weight: bold; font-size:100%;');
+			//dateColumn.setAttribute('style', 'font-weight: bold; font-size:100%;');
 			if (weather.current_weather[0].temp < 40){
-			dateColumn.classList.add("bright","lowtemp");
+			dateColumn.classList.add("small","bright","lowtemp");
 	        dateColumn.innerHTML = weather.current_weather[0].temp + "&#730;";	
 			} else if (weather.current_weather[0].temp > 85){
-			dateColumn.classList.add("bright", "hitemp");
+			dateColumn.classList.add("small","bright", "hitemp");
 	        dateColumn.innerHTML = weather.current_weather[0].temp + "&#730;";	
 			} else {
-			dateColumn.classList.add("bright");
+			dateColumn.classList.add("small","bright");
 	        dateColumn.innerHTML = weather.current_weather[0].temp + "&#730;";	
 			}
 			row.appendChild(dateColumn);
 			weatherTable.appendChild(row);
 
 			var highColumn = document.createElement("td");
-			highColumn.setAttribute('style', 'font-weight: bold; color: #FFF; font-size:100%; ');
+			//highColumn.setAttribute('style', 'font-weight: bold; color: #FFF; font-size:100%; ');
 			if (weather.current_weather[0].humidity > 85){
-	        highColumn.classList.add("bright", "humid");
+	        highColumn.classList.add("small","bright", "humid");
 			highColumn.innerHTML = weather.current_weather[0].humidity+"%";	
 			} else {
-			highColumn.classList.add("bright");
+			highColumn.classList.add("small","bright");
 			highColumn.innerHTML = weather.current_weather[0].humidity+"%";	
 			}
 	        highColumn.innerHTML = weather.current_weather[0].humidity+"%";
@@ -306,7 +308,7 @@
 			weatherTable.appendChild(row);
 
 			var lowColumn = document.createElement("td");
-			lowColumn.classList.add("xsmall", "bright");
+			lowColumn.classList.add("small", "bright");
 			lowColumn.setAttribute("colspan", 1);
 			if (weather.current_weather[0].wind[0].dir == "Not Available"){
 			lowColumn.innerHTML = weather.current_weather[0].wind[0].dir;	
@@ -326,14 +328,14 @@
 			
 			
 			var skyText = document.createElement("td");
-			skyText.setAttribute('style', 'font-weight: bold; font-size:80%;');
-			skyText.classList.add("bright");
+			//skyText.setAttribute('style', 'font-weight: bold; font-size:80%;');
+			skyText.classList.add("small","bright");
 			skyText.innerHTML = moment(forecast.date).format('M-D-YYYY');
 			row.appendChild(skyText);
 			weatherTable.appendChild(row);
          	
 			var nextColumn = document.createElement("td");
-			nextColumn.classList.add("xsmall", "bright");
+			nextColumn.classList.add("small", "bright");
 			nextColumn.innerHTML = forecast.day_max_temp+"&#730;/"+forecast.night_min_temp+"&#730;";
 			row.appendChild(nextColumn);
 			weatherTable.appendChild(row);
@@ -342,19 +344,19 @@
             var n = d.getHours();
 			
 			var fCast = document.createElement("td");
-			fCast.classList.add("xsmall", "bright");
+			fCast.classList.add("small", "bright");
 			fCast.innerHTML =forecast.day[0].weather_text;
 			row.appendChild(fCast);
 			weatherTable.appendChild(row);
 			
 			var fhumid = document.createElement("td");
-			fhumid.classList.add("xsmall", "bright");
+			fhumid.classList.add("small", "bright");
 			fhumid.innerHTML =forecast.day[0].humidity+"%";
 			row.appendChild(fhumid);
 			weatherTable.appendChild(row);
 			
 			var Pchance = document.createElement("td");
-			Pchance.classList.add("xsmall", "bright");
+			Pchance.classList.add("small", "bright");
 			Pchance.innerHTML =forecast.day[0].cloudcover+"%";
 			row.appendChild(Pchance);
 			weatherTable.appendChild(row);
@@ -380,6 +382,7 @@
 			
 			var srss = this.srss
 			
+			
 			var sunrise = srss.sunrise;
             var sunset = srss.sunset;
             var utcsunrise = moment.utc(sunrise).toDate();
@@ -396,7 +399,7 @@
 					} else {
 			Rdate.classList.add("bright","small", "pmclock");			
 					}
-			Rdate.innerHTML = "<img src='modules/MMM-OWeather/icons/Sunrise.png' width=24px; height=24px;>&nbsp;&nbsp;&nbsp;"+sunrise+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='modules/MMM-OWeather/icons/sunset.png' width=24px; height=24px;>&nbsp;&nbsp;&nbsp;"+sunset;
+			Rdate.innerHTML = "<img src='modules/MMM-OWeather/icons/Sunrise.png' width=24px; height=24px;>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+sunrise+ " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='modules/MMM-OWeather/icons/sunset.png' width=24px; height=24px;>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+sunset;
 			Daterow.appendChild(Rdate);
 			weatherTable.appendChild(Daterow);
 			
@@ -407,9 +410,11 @@
   
        processWeather: function(data) {
          this.weather = data.weather;
+console.log(this.weather);
          this.loaded = true;
      },
      
+   
      processSRSS: function(data) {
          this.srss = data.results;
          console.log(this.srss);
